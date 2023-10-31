37c34491ccbce0c6e0ba502eb3fec3684c15735c
Ry0s.dev <eliastello.dev@gmail.com>
2023-10-31 20:50:55 -0300

    yea

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ function Header() {
             <img className="flex w-auto h-24 p-5 px-14" src="../public/logo-empresa.png" alt="imagen " />
 
             <nav className="flex flex-row font-sans place-items-center justify-between ">
-                <Link to={`/`}><a href="" className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Inicio</a></Link>
-                <Link to={`/galery`}><a className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Productos</a></Link>
-                <Link to={`/aboutus`}><a className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Nosotros</a></Link>
-                <Link to={`https:/github.com/jmayola/sportshop`}><a className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:after:scale-x-100 ">Contactos</a></Link>
+                <Link to={`/`}><li href="" className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Inicio</li></Link>
+                <Link to={`/galery`}><li className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Productos</li></Link>
+                <Link to={`/aboutus`}><li className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Nosotros</li></Link>
+                <Link to={`https:/github.com/jmayola/sportshop`}><li className="relative text-sm py-2 text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:after:scale-x-100 ">Contactos</li></Link>
             </nav>
         </header >
     )
