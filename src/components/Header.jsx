f0ad824dda150987d5ecbec77ab5ad9bcd27fefa
Ry0s.dev <eliastello.dev@gmail.com>
2023-11-02 11:35:57 -0300

    Main changes, 2/11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
 function Header() {
     return (
-        <header className=" flex fixed top-0 left-0 w-full px-2 py-0  justify-between align-middle line border-b border-black list-none">
-            <img className="flex w-auto h-28 p-5 px-14" src="../public/logo-empresa.png" alt="imagen " />
+        <header className=" flex fixed top-0 left-0 w-full px-2 py-0 bg-white justify-between align-middle line border-b border-gray-500 list-none">
+            <img className="flex w-auto h-24 p-5 px-14" src="../public/logo-empresa.png" alt="imagen " />
 
             <nav className="flex flex-row font-sans place-items-center justify-between ">
                 <Link to={`/`}><li href="" className="link-animation">Inicio</li></Link>
                 <Link to={`/galery`}><li className=" link-animation">Productos</li></Link>
                 <Link to={`/aboutus`}><li className="link-animation ">Nosotros</li></Link>
+                <Link to={`/register`}><li className="link-animation ">Ingresar</li></Link>
                 <Link to={`https:/github.com/jmayola/sportshop`}><li className="link-animation ">Contactos</li></Link>
             </nav>
         </header >
---
+++ src/components/Products/Presentation.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header";
+
+function Presentation() {
+    return (
+        <>
+            <img src="./public/fondo.jpg" alt="" className="py-24 h-x" />
+            <div className="">
+                <h2 className="">La mejor ropa deportiva para tus entrenamientos</h2>
+            </div>
+        </>
+    )
+}
+
+export default Presentation;
