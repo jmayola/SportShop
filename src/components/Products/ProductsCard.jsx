d8769ebfcd21678353c74b8414085a7c72971bf9
Ry0s.dev <eliastello.dev@gmail.com>
2023-11-06 14:59:17 -0300

    bd changes (SELECT, INSERT, DELETE)

--- src/components/Products/ProductsCard.jsx
+++ src/components/Products/ProductsCard.jsx
@@ -6,103 +6,21 @@ function ProductsCard(children) {
 
   return (
     <>
-      <div className="bg-slate-500 w-max">
+      {/* <div className="bg-slate-500 w-max">
         <p>{Data[index].product_name}</p>
         <p>{Data[index].price}</p>
         <p>{Data[index].mark}</p>
-      </div>
+      </div> */}
 
 
       {/* Sección de productos/cartas */}
-      <section>
-        <div className="flex justify-center p-10 font-inter">
-          <h1 className="font-semibold text-4xl">Productos</h1>
+      <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500 ">
+        <div className="gap-10">
+          <img src="public/botin1.png" alt="" />
+          <h2 className="p-2 font-inter font-bold">{Data[index].product_name}</h2>
+          <span className="font-inter font-bold p-2">{Data[index].price}</span>
         </div>
-        {/* Grid de productos*/}
-        <section className=" grid grid-cols-5 gap-5  ">
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500 ">
-
-            <div>
-              <img src="public/botin1.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500">
-            <div>
-              <img src="public/botin2.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500 ">
-            <div>
-              <img src="public/botin3.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500">
-            <div>
-              <img src="public/camiseta1.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500 ">
-            <div>
-              <img src="public/camiseta2.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500">
-            <div>
-              <img src="public/botin2.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500">
-            <div>
-              <img src="public/botin2.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500">
-            <div>
-              <img src="public/botin2.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500">
-            <div>
-              <img src="public/botin2.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-
-          <div className="flex justify-center p-1 border border-gray-300  hover:shadow-2xl duration-500">
-            <div>
-              <img src="public/botin2.png" alt="" />
-              <h2 className="p-2 font-inter font-bold">Nombre del producto</h2>
-              <span className="font-inter font-bold p-2">$99.999</span>
-            </div>
-          </div>
-        </section>
-      </section>
+      </div>
     </>
   )
 }
--- src/database/db.js
+++ src/database/db.js
@@ -13,28 +13,39 @@ const db = new sqlite.Database(
   }
 );
 // sql = `CREATE TABLE products(id INTEGER PRIMARY KEY, product_name, price, mark)`
-function INSERT() {
+function INSERT(nombre, precio, marca) {
   sql = `INSERT INTO products(product_name, price, mark) VALUES(?,?,?)`;
-  db.run(sql, ["zapas", 200, "adidas"], (err) => {
+  db.run(sql, [nombre, precio, marca], (err) => {
     if (err) return console.error(err.message);
     else console.log("ingreso exitoso");
   });
 }
 function SELECT() {
   sql = `SELECT * FROM products`;
-  db.all(sql,[], (err,rows)=>{
-    if(err) console.error(err.message);
+  db.all(sql, [], (err, rows) => {
+    if (err) console.error(err.message);
     let arr = []
-    rows.forEach(row=>{
-     arr.push(row) //añadimos a un array
+    rows.forEach(row => {
+      arr.push(row) //añadimos a un array
     })
-    app.get("/", (req,res)=>{
+    app.get("/", (req, res) => {
       res.json(arr) // mostramos el array de objetos para visualizarlo
-    })        
+    })
+    console.log("datos cargados en el puerto 3000")
   })
+}
+
+function DELETE(id) {
+  sql = `DELETE FROM products WHERE id=?`;
+  db.run(sql, [id], (err) => {
+    if (err) return console.error(err.message);
+    else console.log("Eliminacion Exitosa");
+  });
 }
 // sql = "DROP TABLE products"
 // db.run(sql)
+// INSERT("Botiness", 79.999, "Adidas")
+
 SELECT()
 app.use(cors())
 app.listen(3000)
