b4cf98ca2b1b0fc700fbbfd5360aa46e8605ef69
mayo <jruso2008@gmail.com>
2023-11-06 13:28:07 -0300

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
 
             <nav className="flex flex-row font-sans place-items-center justify-between ">
                 <Link to={`/`} href className="relative text-sm text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Inicio</Link>
-                <Link to={`/galery`} className="relative text-sm text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Productos</Link>
+                <Link to={`/products`} className="relative text-sm text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Productos</Link>
                 <Link to={`/aboutus`} className="relative text-sm text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2 hover:after:scale-x-100">Nosotros</Link>
                 <Link to={`https:/github.com/jmayola/sportshop`} className="relative text-sm text-white no-underline font-medium mx-3 after:absolute after:left-0 after:bottom-1.5 after:w-full after:h-0.5 after:bg-white after:rounded-md after:scale-x-0 after:transition-transform hover:py-2  hover:after:scale-x-100 hover:after:">Contactos</Link>
             </nav>
