08c5e9789aaf8d15ab7a2a1b80fe167a05425d6d
mayo <jruso2008@gmail.com>
2023-11-11 13:50:02 -0300

    header modificado para responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom"
 function Header() {
     return (
-        <header className=" flex   h-16 w-full px-1 py-0 bg-white justify-between align-middle line border-b border-gray-500 list-none">
-            <img className="flex  px-5 " src="/logo-empresa.png" alt="imagen " />
+        <header className=" flex w-full bg-white justify-between align-middle line border-b border-gray-500 list-none max-lg:flex-col sticky top-0">
+            <img className="w-16 max-lg:m-auto" src="/logo-empresa.png" alt="imagen " />
 
             {/* Hola gente */}
-            <nav className="flex flex-row font-sans place-items-center justify-between px-10 ">
+            <nav className="flex flex-row font-sans place-items-center  justify-between px-10 max-lg:flex-col">
                 <Link to={`/`}><li href="" className="link-animation link-typography text-black text-sm font-inter">Inicio</li></Link>
                 <Link to={`/products`}><li className=" link-animation link-typography font-inter">Productos</li></Link>
                 <Link to={`#aboutus`}><li className="link-animation link-typography font-inter">Nosotros</li></Link>
-                <Link to={`https:/github.com/jmayola/sportshop`}><li className="link-animation link-typography font-inter">Contactos</li></Link>
+                <Link to={`https:/github.com/jmayola/sportshop`}><li className="link-animation link-typography font-inter">Github</li></Link>
                 <Link to={`/register`}><li className="link-animation link-typography font-inter">Ingresar</li></Link>
             </nav>
         </header >
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from './App.jsx'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-  <React.StrictMode>
-    <div id='bg' className='flex min-h-screen flex-col justify-between'>
+    <div id='bg' className='flex min-h-screen flex-col justify-between overflow-x-hidden'>
     <App></App>
     </div>
-  </React.StrictMode>,
 )
