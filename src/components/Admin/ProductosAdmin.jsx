d8f39a393e0194e7c63df582ba49ca09f349bb4a
Ry0s.dev <eliastello.dev@gmail.com>
2023-11-22 13:39:41 -0300

    asd

--- src/components/Admin/ProductosAdmin.jsx
+++ src/components/Admin/ProductosAdmin.jsx
@@ -38,7 +38,7 @@ function ProductosAdmin() {
                   <input
                     name="usuario"
                     type="text"
-                    className="block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+                    className="block w-full  rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
                   />
                 </div>
                 <div className="w-full">
