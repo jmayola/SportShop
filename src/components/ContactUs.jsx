7be9e134b70660e5ed9a80e792e25fb837ec97f1
Ry0s <eliastello.dev@gmail.com>
2024-03-30 23:56:48 -0300

    Contact modified

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,32 +2,29 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "./Footer";
-import { Form, redirect } from "react-router-dom";
 function ContactUs() {
   return (
     <>
       <Header />
-      <section className="contenedor ">
-        <div className="mx-auto flex flex-col items-start justify-start pb-28 pt-10 sm:max-w-[400px] sm:items-center sm:justify-center sm:pt-20 ">
-          <h1 className="mb-2 text-3xl font-semibold sm:mb-5 sm:text-5xl">
-            Contactanos
-          </h1>
+      <section className="flex flex-col items-center">
+        <div className=" py-8">
+          <h1 className="mb-4 text-5xl font-semibold">Contáctanos</h1>
           <form
             method="post"
             action="/Ingresar"
             className="flex flex-col w-full"
           >
-            <div className="flex place-content-center place-items-center flex-col">
+            <div className="flex flex-col gap-y-3">
               <input
                 name="name"
                 type="text"
-                className="block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+                className="rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-1 focus:ring-black"
                 placeholder="Nombre completo"
               />
               <input
                 type="email"
                 name="email"
-                className="block w-full rounded-lg border border-gray-300 px-3 py-2 my-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1 "
+                className="rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-1 focus:ring-black"
                 placeholder="Email"
               />
               <textarea
@@ -36,13 +33,12 @@ function ContactUs() {
                 cols="30"
                 rows="10"
                 placeholder="Mensaje"
-                className="block w-full rounded-lg border border-gray-300 px-3 py-2 my-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1 "
+                className="rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-1 focus:ring-black"
               ></textarea>
+              <button className="flex justify-center rounded-lg bg-black py-3 text-sm font-semibold text-white outline-none focus:ring-1 focus:ring-black">
+                Enviar
+              </button>
             </div>
-
-            <button className="inline-flex w-full items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400">
-              Enviar
-            </button>
           </form>
         </div>
       </section>
