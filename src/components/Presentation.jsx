040fdc7583a575c0b6c99c0cd9cd528838677aa6
Ry0s <eliastello.dev@gmail.com>
2024-04-12 19:22:14 -0300

    Responsive presentation section

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,7 +1,7 @@
 function Presentation() {
   return (
     <div className="w-full h-150 bg-no-repeat bg-cover bg-center bg-[url('/public/Images-home/back-image.jpg')]">
-      <h2 className="grid my-48 mx-2 text text-start text-4xl text-white font-semibold sm:text-center">
+      <h2 className="grid my-48 mx-4 text text-start text-4xl text-white font-semibold sm:text-center">
         La mejor ropa deportiva para tus entrenamientos
       </h2>
     </div>
